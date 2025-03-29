82618b7c8773c3bf13733fe668f1ad5637926326
Alex <alexb158@icloud.com>
2025-03-29 20:36:24 +0000

    Queue refactored ready for virtualisation

--- src/js/components/Queue/Queue.js
+++ src/js/components/Queue/Queue.js
@@ -12,13 +12,18 @@ import { useGetQueuedTracks } from 'js/hooks';
 import style from './Queue.module.scss';
 
 // ======================================================================
+// OPTIONS
+// ======================================================================
+
+// const isLocal = process.env.REACT_APP_ENV === 'local';
+
+// const virtualThreshold = !isLocal ? 150 : 150;
+
+// ======================================================================
 // COMPONENT
 // ======================================================================
 
 const Queue = () => {
-  const optionShowFullTitles_Deprecated = useSelector(
-    ({ sessionModel }) => sessionModel.optionShowFullTitles_Deprecated
-  );
   const queueExpandArtwork = useSelector(({ sessionModel }) => sessionModel.queueExpandArtwork);
 
   const {
@@ -32,167 +37,89 @@ const Queue = () => {
     upcomingEntries,
 
     repeatEntries,
-    totalTracksRemaining,
+    // totalTracksRemaining,
   } = useGetQueuedTracks();
+
+  const currentTrack = { rowType: 'playing', playIndex: playingTrackIndex, ...upcomingEntries[0] };
+  const upcomingTracks = upcomingEntries
+    .filter((entry, index) => index > 0)
+    .map((entry, index) => {
+      return { rowType: 'upcoming', playIndex: index + playingTrackIndex + 1, ...entry };
+    });
+  const repeatTracks = repeatEntries.map((entry, index) => {
+    return { rowType: 'repeat', playIndex: index, ...entry };
+  });
+  const upcomingLabel = upcomingTracks.length > 0 || repeatTracks.length > 0 ? [{ rowType: 'upcomingLabel' }] : [];
+  const repeatLabel = playingRepeat ? [{ rowType: 'repeatLabel' }] : [];
+
+  const allEntries = [currentTrack, ...upcomingLabel, ...upcomingTracks, ...repeatTracks, ...repeatLabel];
 
   return (
     <div className={style.wrap}>
       <div className={style.inner}>
         {!playingTrackKeys && <QueueEmpty />}
         {playingTrackKeys && (
-          <QueueList
-            entries={upcomingEntries}
-            initialIndex={playingTrackIndex}
-            isRepeat={false}
-            optionShowFullTitles_Deprecated={optionShowFullTitles_Deprecated}
-            playingShuffle={playingShuffle}
-            queueExpandArtwork={queueExpandArtwork}
-            totalTracksRemaining={totalTracksRemaining}
-          />
-        )}
-        {playingRepeat && (
-          <>
-            <QueueList
-              entries={repeatEntries}
-              initialIndex={0}
-              isRepeat={true}
-              optionShowFullTitles_Deprecated={optionShowFullTitles_Deprecated}
-              totalTracksRemaining={totalTracksRemaining}
-            />
-            <div className={style.repeat}>
-              <span>Repeating</span>
-            </div>
-          </>
+          <QueueStatic entries={allEntries} playingShuffle={playingShuffle} queueExpandArtwork={queueExpandArtwork} />
         )}
       </div>
     </div>
   );
 };
 
+// ======================================================================
+// QUEUE - EMPTY
+// ======================================================================
+
 const QueueEmpty = () => {
   return <div className={style.section}>No tracks in queue</div>;
 };
 
-const QueueList = ({
-  entries,
-  initialIndex = 0,
-  isRepeat,
-  optionShowFullTitles_Deprecated,
-  playingShuffle,
-  queueExpandArtwork,
-  totalTracksRemaining,
-}) => {
-  return entries.map((entry, index) => {
-    const isCurrentlyPlaying = index === 0;
+// ======================================================================
+// QUEUE - STATIC
+// ======================================================================
 
-    return (
-      <QueueEntry
-        key={entry.trackId}
-        index={index + initialIndex}
-        entry={entry}
-        isRepeat={isRepeat}
-        isCurrentlyPlaying={isCurrentlyPlaying}
-        totalTracksRemaining={totalTracksRemaining}
-        optionShowFullTitles_Deprecated={optionShowFullTitles_Deprecated}
-        queueExpandArtwork={queueExpandArtwork}
-        playingShuffle={playingShuffle}
-      />
-    );
-  });
-};
-
-const QueueEntry = ({
-  entry,
-  index,
-  isRepeat,
-  isCurrentlyPlaying,
-  optionShowFullTitles_Deprecated,
-  playingShuffle,
-  queueExpandArtwork = false,
-  totalTracksRemaining,
-}) => {
-  const dispatch = useDispatch();
-
-  const doPlay = () => {
-    if (!isCurrentlyPlaying) {
-      dispatch.playerModel.playerLoadIndex({ index: index, play: true });
-    }
-  };
-
-  const expandArtwork = () => {
-    dispatch.sessionModel.setSessionState({ queueExpandArtwork: true });
-  };
-
+const QueueStatic = ({ entries, playingShuffle, queueExpandArtwork }) => {
   return (
     <>
-      {!isRepeat && isCurrentlyPlaying && (
-        <>
-          {queueExpandArtwork && (
-            <QueueEntryExpanded entry={entry} optionShowFullTitles_Deprecated={optionShowFullTitles_Deprecated} />
-          )}
-          {!queueExpandArtwork && (
-            <button className={style.section} onClick={expandArtwork}>
-              Now playing
-              <span className={style.expandIcon}>
-                <Icon icon="ExpandIcon" cover stroke strokeWidth={1.4} />
-              </span>
-            </button>
-          )}
-        </>
-      )}
+      {entries.map((entry, index) => {
+        // Catch missing entries
+        if (!entry) {
+          return null;
+        }
 
-      {(!isCurrentlyPlaying || !queueExpandArtwork) && (
-        <div
-          className={clsx(style.entry, {
-            [style.entryCurrent]: !isRepeat && isCurrentlyPlaying,
-            'text-trim': !optionShowFullTitles_Deprecated,
-          })}
-          onDoubleClick={() => {
-            doPlay(true);
-          }}
-          onKeyDown={(event) => {
-            if (event.key === 'Enter') {
-              doPlay(true);
-            }
-          }}
-          tabIndex={0}
-        >
-          <div className={style.thumb}>
-            {entry.thumb && <img src={entry.thumb} alt={entry.title} loading="lazy" draggable="false" />}
-          </div>
+        // Now playing
+        else if (entry.rowType === 'playing') {
+          if (queueExpandArtwork) {
+            return <NowPlayingLarge key={index} entry={entry} />;
+          } else {
+            return <NowPlayingSmall key={index} entry={entry} />;
+          }
+        }
 
-          <div className={clsx(style.content, { 'text-trim': !optionShowFullTitles_Deprecated })}>
-            <div className={clsx(style.title, { 'text-trim': !optionShowFullTitles_Deprecated })}>{entry.title}</div>
-            <div className={clsx(style.artist, { 'text-trim': !optionShowFullTitles_Deprecated })}>
-              {entry.artistLink && (
-                <NavLink to={entry.artistLink} tabIndex={-1} draggable="false">
-                  {entry.artist}
-                </NavLink>
-              )}
-              {!entry.artistLink && entry.artist}
-            </div>
-          </div>
-        </div>
-      )}
+        // Label - Upcoming
+        else if (entry.rowType === 'upcomingLabel') {
+          return <LabelUpcoming key={index} playingShuffle={playingShuffle} />;
+        }
 
-      {!isRepeat && isCurrentlyPlaying && totalTracksRemaining > 1 && (
-        <div className={style.section}>
-          Coming up
-          {playingShuffle && (
-            <span className={style.shuffleLabel}>
-              &nbsp;&nbsp;•&nbsp; Shuffle is on{' '}
-              <span className={style.shuffleIcon}>
-                <Icon icon="ShuffleIcon" cover stroke />
-              </span>
-            </span>
-          )}
-        </div>
-      )}
+        // Label - Repeat
+        else if (entry.rowType === 'repeatLabel') {
+          return <LabelRepeat key={index} />;
+        }
+
+        // Tracks
+        else {
+          return <TrackEntry key={index} entry={entry} />;
+        }
+      })}
     </>
   );
 };
 
-const QueueEntryExpanded = ({ entry, optionShowFullTitles_Deprecated }) => {
+// ======================================================================
+// NOW PLAYING - LARGE
+// ======================================================================
+
+const NowPlayingLarge = ({ entry }) => {
   const dispatch = useDispatch();
 
   const collapseArtwork = () => {
@@ -219,9 +146,9 @@ const QueueEntryExpanded = ({ entry, optionShowFullTitles_Deprecated }) => {
         </button>
       </div>
 
-      <div className={clsx(style.expandedTitle, { 'text-trim': !optionShowFullTitles_Deprecated })}>{entry.title}</div>
+      <div className={clsx(style.expandedTitle, 'text-trim')}>{entry.title}</div>
 
-      <div className={clsx(style.expandedArtist, { 'text-trim': !optionShowFullTitles_Deprecated })}>
+      <div className={clsx(style.expandedArtist, 'text-trim')}>
         {entry.artistLink && (
           <NavLink draggable="false" to={entry.artistLink} tabIndex={-1}>
             {entry.artist}
@@ -229,6 +156,109 @@ const QueueEntryExpanded = ({ entry, optionShowFullTitles_Deprecated }) => {
         )}
         {!entry.artistLink && entry.artist}
       </div>
+    </div>
+  );
+};
+
+// ======================================================================
+// NOW PLAYING - SMALL
+// ======================================================================
+
+const NowPlayingSmall = ({ entry }) => {
+  const dispatch = useDispatch();
+
+  const expandArtwork = () => {
+    dispatch.sessionModel.setSessionState({ queueExpandArtwork: true });
+  };
+
+  return (
+    <>
+      <button className={style.section} onClick={expandArtwork}>
+        Now playing
+        <span className={style.expandIcon}>
+          <Icon icon="ExpandIcon" cover stroke strokeWidth={1.4} />
+        </span>
+      </button>
+      <TrackEntry entry={entry} isCurrentlyPlaying={true} />
+    </>
+  );
+};
+
+// ======================================================================
+// TRACK ENTRY
+// ======================================================================
+
+const TrackEntry = ({ entry, isCurrentlyPlaying = false }) => {
+  const dispatch = useDispatch();
+
+  const doPlay = () => {
+    dispatch.playerModel.playerLoadIndex({ index: entry.playIndex, play: true });
+  };
+
+  return (
+    <>
+      <div
+        className={clsx(style.entry, 'text-trim', {
+          [style.entryCurrent]: isCurrentlyPlaying,
+        })}
+        onDoubleClick={() => {
+          doPlay(true);
+        }}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter') {
+            doPlay(true);
+          }
+        }}
+        tabIndex={0}
+      >
+        <div className={style.thumb}>
+          {entry.thumb && <img src={entry.thumb} alt={entry.title} loading="lazy" draggable="false" />}
+        </div>
+
+        <div className={clsx(style.content, 'text-trim')}>
+          <div className={clsx(style.title, 'text-trim')}>{entry.title}</div>
+          <div className={clsx(style.artist, 'text-trim')}>
+            {entry.artistLink && (
+              <NavLink to={entry.artistLink} tabIndex={-1} draggable="false">
+                {entry.artist}
+              </NavLink>
+            )}
+            {!entry.artistLink && entry.artist}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+// ======================================================================
+// LABEL - UPCOMING
+// ======================================================================
+
+const LabelUpcoming = ({ playingShuffle }) => {
+  return (
+    <div className={style.section}>
+      Coming up
+      {playingShuffle && (
+        <span className={style.shuffleLabel}>
+          &nbsp;&nbsp;•&nbsp; Shuffle is on{' '}
+          <span className={style.shuffleIcon}>
+            <Icon icon="ShuffleIcon" cover stroke />
+          </span>
+        </span>
+      )}
+    </div>
+  );
+};
+
+// ======================================================================
+// LABEL - REPEAT
+// ======================================================================
+
+const LabelRepeat = () => {
+  return (
+    <div className={style.repeat}>
+      <span>Repeating</span>
     </div>
   );
 };
