702a814eb0ad3acb6117062491c85dfbb0bb3236
Alex <alexb158@icloud.com>
2025-05-11 18:12:57 +0100

    Re-implement new user menu design (in progress)

--- src/js/components/UserMenu/UserMenu.js
+++ src/js/components/UserMenu/UserMenu.js
@@ -2,13 +2,14 @@
 // IMPORTS
 // ======================================================================
 
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import * as RadixMenu from '@radix-ui/react-dropdown-menu';
 import clsx from 'clsx';
 
 import { Icon } from 'js/components';
-import { appPlatform } from 'js/utils';
+// import { appPlatform } from 'js/utils';
 
 import style from './UserMenu.module.scss';
 
@@ -19,7 +20,7 @@ import style from './UserMenu.module.scss';
 const UserMenu = ({ variant = 'default' }) => {
   const dispatch = useDispatch();
 
-  const [showMenu, setShowMenu] = useState(false);
+  // const [showMenu, setShowMenu] = useState(false);
 
   const currentUser = useSelector(({ appModel }) => appModel.currentUser);
   const currentServer = useSelector(({ sessionModel }) => sessionModel.currentServer);
@@ -32,13 +33,13 @@ const UserMenu = ({ variant = 'default' }) => {
   const hasSelectedLibrary = currentServer && currentLibrary;
   const hasQueueVisible = queueIsVisible && hasSelectedLibrary;
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+  // const toggleMenu = () => {
+  //   setShowMenu(!showMenu);
+  // };
 
   return (
     <>
-      {showMenu && appPlatform !== 'win' && <div className={style.overlay} onClick={toggleMenu}></div>}
+      {/* {showMenu && appPlatform !== 'win' && <div className={style.overlay} onClick={toggleMenu}></div>} */}
 
       <div
         className={clsx(style.wrap, style[`wrap${variant}`], {
@@ -47,115 +48,140 @@ const UserMenu = ({ variant = 'default' }) => {
           [style.wrapWithQueue]: hasQueueVisible,
         })}
       >
-        <button className={style.status} onClick={toggleMenu}>
-          {hasSelectedLibrary && (
-            <div className={style.content}>
-              <div className={style.library}>{currentLibrary.title}</div>
-              <div className={style.server}>{currentServer.name}</div>
-            </div>
-          )}
-          <div className={style.thumb}>
-            {!currentUser.thumb && (
-              <span className={style.thumbIcon}>
-                <Icon icon="ArtistCollectionsIcon" cover stroke strokeWidth={1.4} />
-              </span>
-            )}
-            {currentUser.thumb && <img src={currentUser.thumb} alt={currentUser.title} draggable="false" />}
-          </div>
-        </button>
-
-        {showMenu && (
-          <div className={style.menu}>
+        <RadixMenu.Root
+        // open
+        >
+          <RadixMenu.Trigger
+            className={style.status}
+            // onClick={toggleMenu}
+          >
             {hasSelectedLibrary && (
-              <>
-                {allServers &&
-                  allServers.map((server) => (
-                    <React.Fragment key={server.serverId}>
-                      <button
-                        className={clsx(style.button, style.buttonServer)}
-                        onClick={() => {
-                          dispatch.sessionModel.switchCurrentServer(server.serverId);
-                          toggleMenu();
-                        }}
-                      >
-                        <span className={style.iconBefore}>
-                          <Icon icon="ServerIcon" cover stroke />
-                        </span>
-                        {server.name}
-                        <span className={clsx(style.iconArrow, style.iconHover)}>
-                          <Icon icon="NextIcon" cover stroke />
-                        </span>
-                      </button>
-
-                      {server.serverId === currentServer.serverId &&
-                        allLibraries &&
-                        allLibraries.map((library) => {
-                          const isCurrentLibrary = library.libraryId === currentLibrary.libraryId;
-                          return (
-                            <button
-                              key={library.libraryId}
-                              className={clsx(style.button, style.buttonLibrary)}
-                              onClick={() => {
-                                dispatch.sessionModel.switchCurrentLibrary(library.libraryId);
-                                toggleMenu();
-                              }}
-                            >
-                              <span className={style.iconBefore}>
-                                <Icon icon="MusicNoteDoubleIcon" cover stroke />
-                              </span>
-                              {library.title}
-                              {isCurrentLibrary && (
-                                <span className={clsx(style.iconAfter, style.iconCurrent)}>
-                                  <Icon icon="CheckCircleFilledIcon" cover />
-                                </span>
-                              )}
-                              {!isCurrentLibrary && (
-                                <span className={clsx(style.iconAfter, style.iconHover)}>
-                                  <Icon icon="CheckCircleCheckedIcon" cover stroke />
-                                </span>
-                              )}
-                            </button>
-                          );
-                        })}
-                    </React.Fragment>
-                  ))}
-
-                <NavLink
-                  className={style.button}
-                  to={'/settings'}
-                  onClick={() => {
-                    toggleMenu();
-                  }}
-                  draggable="false"
-                >
-                  <span className={style.iconBefore}>
-                    <Icon icon="CogIcon" cover stroke />
-                  </span>
-                  Settings
-                  <span className={clsx(style.iconArrow, style.iconHover)}>
-                    <Icon icon="NextIcon" cover stroke />
-                  </span>
-                </NavLink>
-              </>
+              <div className={style.content}>
+                <div className={style.library}>{currentLibrary.title}</div>
+                <div className={style.server}>{currentServer.name}</div>
+              </div>
             )}
+            <div className={style.thumb}>
+              {!currentUser.thumb && (
+                <span className={style.thumbIcon}>
+                  <Icon icon="ArtistCollectionsIcon" cover stroke strokeWidth={1.4} />
+                </span>
+              )}
+              {currentUser.thumb && <img src={currentUser.thumb} alt={currentUser.title} draggable="false" />}
+            </div>
+          </RadixMenu.Trigger>
 
-            <button
-              className={style.button}
-              onClick={() => {
-                dispatch.appModel.doLogout();
-                toggleMenu();
-              }}
-            >
-              <span className={style.iconBefore}>
-                <Icon icon="LogoutIcon" cover stroke />
-              </span>
-              Logout
-              <span className={clsx(style.iconArrow, style.iconHover)}>
-                <Icon icon="NextIcon" cover stroke />
-              </span>
-            </button>
-          </div>
-        )}
+          <RadixMenu.Portal>
+            <RadixMenu.Content side="bottom" align="end" className={style.menu}>
+              {hasSelectedLibrary && allServers && (
+                <>
+                  <RadixMenu.Group>
+                    <RadixMenu.Label className={style.label}>Plex</RadixMenu.Label>
+
+                    {allServers.map((server) => (
+                      <React.Fragment key={server.serverId}>
+                        <RadixMenu.Item asChild>
+                          <button
+                            className={clsx(style.button, style.buttonServer)}
+                            onClick={() => {
+                              dispatch.sessionModel.switchCurrentServer(server.serverId);
+                              // toggleMenu();
+                            }}
+                          >
+                            <span className={style.iconBefore}>
+                              <Icon icon="ServerIcon" cover stroke />
+                            </span>
+                            {server.name}
+                            <span className={clsx(style.iconArrow, style.iconHover)}>
+                              <Icon icon="NextIcon" cover stroke />
+                            </span>
+                          </button>
+                        </RadixMenu.Item>
+
+                        {server.serverId === currentServer.serverId &&
+                          allLibraries &&
+                          allLibraries.map((library) => {
+                            const isCurrentLibrary = library.libraryId === currentLibrary.libraryId;
+                            return (
+                              <RadixMenu.Item key={library.libraryId} asChild>
+                                <button
+                                  className={clsx(style.button, style.buttonLibrary, {
+                                    [style.buttonCurrent]: isCurrentLibrary,
+                                  })}
+                                  onClick={() => {
+                                    dispatch.sessionModel.switchCurrentLibrary(library.libraryId);
+                                    // toggleMenu();
+                                  }}
+                                >
+                                  <span className={style.iconBefore}>
+                                    <Icon icon="MusicNoteDoubleIcon" cover stroke />
+                                  </span>
+                                  {library.title}
+                                  {isCurrentLibrary && (
+                                    <span className={clsx(style.iconAfter, style.iconCurrent)}>
+                                      <Icon icon="CheckCircleFilledIcon" cover />
+                                    </span>
+                                  )}
+                                  {!isCurrentLibrary && (
+                                    <span className={clsx(style.iconAfter, style.iconHover)}>
+                                      <Icon icon="CheckCircleCheckedIcon" cover stroke />
+                                    </span>
+                                  )}
+                                </button>
+                              </RadixMenu.Item>
+                            );
+                          })}
+                      </React.Fragment>
+                    ))}
+                  </RadixMenu.Group>
+
+                  <RadixMenu.Separator className={style.separator} />
+                </>
+              )}
+
+              <RadixMenu.Group>
+                {hasSelectedLibrary && allServers && (
+                  <RadixMenu.Item asChild>
+                    <NavLink
+                      className={style.button}
+                      to={'/settings'}
+                      // onClick={() => {
+                      //   toggleMenu();
+                      // }}
+                      draggable="false"
+                    >
+                      <span className={style.iconBefore}>
+                        <Icon icon="CogIcon" cover stroke />
+                      </span>
+                      Settings
+                      <span className={clsx(style.iconArrow, style.iconHover)}>
+                        <Icon icon="NextIcon" cover stroke />
+                      </span>
+                    </NavLink>
+                  </RadixMenu.Item>
+                )}
+
+                <RadixMenu.Item asChild>
+                  <button
+                    className={style.button}
+                    onClick={() => {
+                      dispatch.appModel.doLogout();
+                      // toggleMenu();
+                    }}
+                  >
+                    <span className={style.iconBefore}>
+                      <Icon icon="LogoutIcon" cover stroke />
+                    </span>
+                    Logout
+                    <span className={clsx(style.iconArrow, style.iconHover)}>
+                      <Icon icon="NextIcon" cover stroke />
+                    </span>
+                  </button>
+                </RadixMenu.Item>
+              </RadixMenu.Group>
+            </RadixMenu.Content>
+          </RadixMenu.Portal>
+        </RadixMenu.Root>
       </div>
     </>
   );
