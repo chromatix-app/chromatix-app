f33802776f4dc554bcf8592a3040f910e89e8916
Alex <alexb158@icloud.com>
2025-07-25 12:06:52 +0100

    Appearance settings tidying

--- src/js/components/SettingsAppearance/SettingsAppearance.jsx
+++ src/js/components/SettingsAppearance/SettingsAppearance.jsx
@@ -16,15 +16,27 @@ import style from './SettingsAppearance.module.scss';
 // ======================================================================
 
 export const SettingsAppearance = () => {
+  const osName = getOperatingSystemName();
+  const isWin = osName === 'Windows' || (isElectron && electronPlatform === 'win');
+
+  return (
+    <div className={style.wrap}>
+      <PresetThemeSettings />
+      <CustomThemeSettings />
+      <DisplaySettings />
+      {isWin && <ScrollbarSettings />}
+    </div>
+  );
+};
+
+//
+// PRESET THEMES
+//
+
+const PresetThemeSettings = () => {
   const dispatch = useDispatch();
 
   const currentTheme = useSelector(({ sessionModel }) => sessionModel.currentTheme);
-  const currentColorBackground = useSelector(({ sessionModel }) => sessionModel.currentColorBackground);
-  const currentColorText = useSelector(({ sessionModel }) => sessionModel.currentColorText);
-  const currentColorPrimary = useSelector(({ sessionModel }) => sessionModel.currentColorPrimary);
-
-  const osName = getOperatingSystemName();
-  const isWin = osName === 'Windows' || (isElectron && electronPlatform === 'win');
 
   const groupedThemes = Object.entries(themes).reduce((groups, [themeName, themeDetails]) => {
     const group = themeDetails.group;
@@ -35,6 +47,45 @@ export const SettingsAppearance = () => {
     return groups;
   }, {});
 
+  return Object.entries(groupedThemes).map(([groupName, groupThemes], groupIndex) => (
+    <div key={groupIndex} className={style.group}>
+      <div className={style.title}>{groupName}</div>
+      <div className={style.themes}>
+        {groupThemes.map(([themeName, themeDetails], themeIndex) => (
+          <button
+            key={themeIndex}
+            className={clsx(style.theme, {
+              [style.themeCurrent]: currentTheme === themeName,
+            })}
+            onClick={() => {
+              dispatch.sessionModel.setTheme(themeName);
+            }}
+          >
+            <div className={style.themeBackground} style={{ background: themeDetails.background }}>
+              <div
+                className={style.themeText}
+                style={{ borderColor: `transparent transparent ${themeDetails.primary} transparent` }}
+              ></div>
+            </div>
+          </button>
+        ))}
+      </div>
+    </div>
+  ));
+};
+
+//
+// CUSTOM THEME
+//
+
+const CustomThemeSettings = () => {
+  const dispatch = useDispatch();
+
+  const currentTheme = useSelector(({ sessionModel }) => sessionModel.currentTheme);
+  const currentColorBackground = useSelector(({ sessionModel }) => sessionModel.currentColorBackground);
+  const currentColorText = useSelector(({ sessionModel }) => sessionModel.currentColorText);
+  const currentColorPrimary = useSelector(({ sessionModel }) => sessionModel.currentColorPrimary);
+
   const resetCustomTheme = () => {
     dispatch.sessionModel.setColorBackground(themes.chromatix.background);
     dispatch.sessionModel.setColorText(themes.chromatix.text);
@@ -42,102 +93,71 @@ export const SettingsAppearance = () => {
   };
 
   return (
-    <div className={style.wrap}>
-      {Object.entries(groupedThemes).map(([groupName, groupThemes], groupIndex) => (
-        <div key={groupIndex} className={style.group}>
-          <div className={style.title}>{groupName}</div>
-          <div className={style.themes}>
-            {groupThemes.map(([themeName, themeDetails], themeIndex) => (
-              <button
-                key={themeIndex}
-                className={clsx(style.theme, {
-                  [style.themeCurrent]: currentTheme === themeName,
-                })}
-                onClick={() => {
-                  dispatch.sessionModel.setTheme(themeName);
-                }}
-              >
-                <div className={style.themeBackground} style={{ background: themeDetails.background }}>
-                  <div
-                    className={style.themeText}
-                    style={{ borderColor: `transparent transparent ${themeDetails.primary} transparent` }}
-                  ></div>
-                </div>
-              </button>
-            ))}
+    <div className={style.group}>
+      <div className={style.title}>Custom theme</div>
+      <div className={style.themes}>
+        <button
+          className={clsx(style.theme, {
+            [style.themeCurrent]: currentTheme === 'custom',
+          })}
+          onClick={() => {
+            dispatch.sessionModel.setTheme('custom');
+          }}
+        >
+          <div className={style.themeBackground}>
+            <div className={style.icon}>
+              <Icon icon="PencilIcon" cover stroke />
+            </div>
           </div>
-        </div>
-      ))}
+        </button>
 
-      <div className={style.group}>
-        <div className={style.title}>Custom theme</div>
-        <div className={style.themes}>
-          <button
-            className={clsx(style.theme, {
-              [style.themeCurrent]: currentTheme === 'custom',
-            })}
-            onClick={() => {
-              dispatch.sessionModel.setTheme('custom');
-            }}
-          >
-            <div className={style.themeBackground}>
-              <div className={style.icon}>
-                <Icon icon="PencilIcon" cover stroke />
-              </div>
+        {currentTheme === 'custom' && (
+          <div className={style.custom}>
+            <div className={style.customField}>
+              <div className={style.customLabel}>Background:</div>
+              <input
+                type="color"
+                className={style.customInput}
+                value={currentColorBackground}
+                onChange={(event) => {
+                  dispatch.sessionModel.setColorBackground(event.target.value);
+                }}
+              />
             </div>
-          </button>
-
-          {currentTheme === 'custom' && (
-            <div className={style.custom}>
-              <div className={style.customField}>
-                <div className={style.customLabel}>Background:</div>
-                <input
-                  type="color"
-                  className={style.customInput}
-                  value={currentColorBackground}
-                  onChange={(event) => {
-                    dispatch.sessionModel.setColorBackground(event.target.value);
-                  }}
-                />
-              </div>
-              <div className={style.customField}>
-                <div className={style.customLabel}>Text:</div>
-                <input
-                  type="color"
-                  className={style.customInput}
-                  value={currentColorText}
-                  onChange={(event) => {
-                    dispatch.sessionModel.setColorText(event.target.value);
-                  }}
-                />
-              </div>
-              <div className={style.customField}>
-                <div className={style.customLabel}>Highlight:</div>
-                <input
-                  type="color"
-                  className={style.customInput}
-                  value={currentColorPrimary}
-                  onChange={(event) => {
-                    dispatch.sessionModel.setColorPrimary(event.target.value);
-                  }}
-                />
-              </div>
-              {(currentColorBackground !== themes.chromatix.background ||
-                currentColorText !== themes.chromatix.text ||
-                currentColorPrimary !== themes.chromatix.primary) && (
-                <div className={style.customField}>
-                  <button className={style.button} onClick={resetCustomTheme}>
-                    Reset
-                  </button>
-                </div>
-              )}
+            <div className={style.customField}>
+              <div className={style.customLabel}>Text:</div>
+              <input
+                type="color"
+                className={style.customInput}
+                value={currentColorText}
+                onChange={(event) => {
+                  dispatch.sessionModel.setColorText(event.target.value);
+                }}
+              />
             </div>
-          )}
-        </div>
+            <div className={style.customField}>
+              <div className={style.customLabel}>Highlight:</div>
+              <input
+                type="color"
+                className={style.customInput}
+                value={currentColorPrimary}
+                onChange={(event) => {
+                  dispatch.sessionModel.setColorPrimary(event.target.value);
+                }}
+              />
+            </div>
+            {(currentColorBackground !== themes.chromatix.background ||
+              currentColorText !== themes.chromatix.text ||
+              currentColorPrimary !== themes.chromatix.primary) && (
+              <div className={style.customField}>
+                <button className={style.button} onClick={resetCustomTheme}>
+                  Reset
+                </button>
+              </div>
+            )}
+          </div>
+        )}
       </div>
-
-      {!isWin && <ScrollbarSettings />}
-      <DisplaySettings />
     </div>
   );
 };
