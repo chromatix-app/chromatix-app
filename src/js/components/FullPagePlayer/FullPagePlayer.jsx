d58b5b58cd183c60e6cf194b151bc37ce5e82df5
Alex <alexb158@icloud.com>
2025-08-18 22:57:30 +0100

    More fields added to full page player

--- src/js/components/FullPagePlayer/FullPagePlayer.jsx
+++ src/js/components/FullPagePlayer/FullPagePlayer.jsx
@@ -5,7 +5,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import clsx from 'clsx';
 
+import platformFeatures from 'js/_config/platformFeatures';
 import { ControlProgress, PrimaryControls, SecondaryControls } from 'js/components/ControlBar/ControlBar';
+import { Favourite, StarRating } from 'js/components';
 import { useKeyControl } from 'js/hooks';
 
 import style from './FullPagePlayer.module.scss';
@@ -47,20 +49,24 @@ export const FullPagePlayer = () => {
 };
 
 const NowPlaying = () => {
+  const currentService = useSelector(({ appModel }) => appModel.currentService);
+
   const playingTrackList = useSelector(({ sessionModel }) => sessionModel.playingTrackList);
   const playingTrackIndex = useSelector(({ sessionModel }) => sessionModel.playingTrackIndex);
   const playingTrackKeys = useSelector(({ sessionModel }) => sessionModel.playingTrackKeys);
 
   const trackCurrent = playingTrackList?.[playingTrackKeys[playingTrackIndex]];
 
+  const platformOpts = platformFeatures[currentService] || {};
+
   return (
     <div className={style.nowPlaying}>
       <div className={clsx(style.cover, { [style.coverPlaceholder]: !trackCurrent || !trackCurrent?.thumb })}>
         {trackCurrent && (
           <>
-            {trackCurrent.thumb && (
+            {(trackCurrent.thumbMedium || trackCurrent.thumb) && (
               <div className={style.coverArtwork}>
-                <img src={trackCurrent.thumb} alt={trackCurrent.title} draggable="false" />
+                <img src={trackCurrent.thumbMedium || trackCurrent.thumb} alt={trackCurrent.title} draggable="false" />
               </div>
             )}
           </>
@@ -72,6 +78,32 @@ const NowPlaying = () => {
             <div className={clsx(style.title, 'text-trim')}>{trackCurrent.title}</div>
             <div className={clsx(style.artist, 'text-trim')}>{trackCurrent.artist}</div>
             <div className={clsx(style.album, 'text-trim')}>{trackCurrent.album}</div>
+            {platformOpts.enableIsFavourite && (
+              <div className={style.favourite}>
+                <Favourite
+                  variant="fullpage"
+                  type="track"
+                  itemId={trackCurrent.trackId}
+                  isFavourite={trackCurrent.isFavourite}
+                  editable
+                />
+              </div>
+            )}
+            {platformOpts.enableUserRating && (
+              <div className={style.rating}>
+                <StarRating
+                  variant="fullpage"
+                  type="track"
+                  ratingKey={trackCurrent.trackId}
+                  rating={trackCurrent.userRating}
+                  size={14}
+                  editable
+                />
+              </div>
+            )}
+            <div className={clsx(style.specs, 'text-trim')}>
+              {trackCurrent.codec} • {trackCurrent.bitrate}kbps
+            </div>
           </>
         )}
       </div>
