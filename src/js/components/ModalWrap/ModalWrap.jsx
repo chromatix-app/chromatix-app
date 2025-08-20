9978705a19c3b4c042d77170b9bbb09cee751cee
Alex <alexb158@icloud.com>
2025-08-20 15:49:11 +0100

    ModalWrap updated with touch scrolling logic

--- src/js/components/ModalWrap/ModalWrap.jsx
+++ src/js/components/ModalWrap/ModalWrap.jsx
@@ -2,8 +2,11 @@
 // IMPORTS
 // ======================================================================
 
+import { useLayoutEffect, useRef, useState } from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import clsx from 'clsx';
+
+import { useWindowSize } from 'js/hooks';
 
 import style from './ModalWrap.module.scss';
 
@@ -12,12 +15,28 @@ import style from './ModalWrap.module.scss';
 // ======================================================================
 
 export const ModalWrap = ({ children, close, open = true }) => {
+  const modalRef = useRef(null);
+  const { windowWidth, windowHeight } = useWindowSize();
+  const [hasOverflow, setHasOverflow] = useState(false);
+
+  useLayoutEffect(() => {
+    if (modalRef.current) {
+      setHasOverflow(modalRef.current.scrollHeight > modalRef.current.clientHeight);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [windowWidth, windowHeight, modalRef.current]);
+
   return (
     <Dialog.Root open={open} onOpenChange={(isOpen) => !isOpen && close()}>
       <Dialog.Portal>
         <Dialog.Overlay className={style.blackout} />
-        <Dialog.Content className={clsx(style.center)} onPointerDownOutside={close}>
-          <div className={style.overflow}>{children}</div>
+        <Dialog.Content
+          className={clsx(style.center, { [style.centerWithOverflow]: hasOverflow })}
+          onPointerDownOutside={close}
+        >
+          <div ref={modalRef} className={style.overflow}>
+            {children}
+          </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
