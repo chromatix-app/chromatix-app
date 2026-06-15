da3571fbfba469d458c196b9420deb00e87fc819
Alex <alexb158@icloud.com>
2026-06-15 22:17:09 +0100

    Playwright updates

--- tests/pages/3.library.auth.spec.ts
+++ tests/pages/3.library.auth.spec.ts
@@ -18,58 +18,62 @@ test.describe('library pages match snapshots', () => {
   });
 
   test('artists page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/artists`, 'artists');
+    await snapshotPage(page, `/libraries/${libraryId}/artists`, '001-artists');
   });
 
   test('artist detail page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/artists/165946`, 'artist-detail');
+    await snapshotPage(page, `/libraries/${libraryId}/artists/165946`, '002-artist-detail');
   });
 
   test('albums page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/albums`, 'albums');
+    await snapshotPage(page, `/libraries/${libraryId}/albums`, '003-albums');
   });
 
   test('album detail page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/albums/165830`, 'album-detail');
+    await snapshotPage(page, `/libraries/${libraryId}/albums/165830`, '004-album-detail');
   });
 
   test('folders page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/folders`, 'folders');
+    await snapshotPage(page, `/libraries/${libraryId}/folders`, '005-folders');
+  });
+
+  test('folder detail page matches snapshot', async ({ page }) => {
+    await snapshotPage(page, `/libraries/${libraryId}/folders/4410`, '006-folder-detail');
   });
 
   // test('playlists page matches snapshot', async ({ page }) => {
-  //   await snapshotPage(page, `/libraries/${libraryId}/playlists`, 'playlists');
+  //   await snapshotPage(page, `/libraries/${libraryId}/playlists`, '006-playlists');
   // });
 
   test('artist collections page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/artist-collections`, 'artist-collections');
+    await snapshotPage(page, `/libraries/${libraryId}/artist-collections`, '007-artist-collections');
   });
 
   test('artist collection detail page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/artist-collections/165431`, 'artist-collection-detail');
+    await snapshotPage(page, `/libraries/${libraryId}/artist-collections/165431`, '008-artist-collection-detail');
   });
 
   test('album collections page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/album-collections`, 'album-collections');
+    await snapshotPage(page, `/libraries/${libraryId}/album-collections`, '009-album-collections');
   });
 
   test('album collection detail page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/album-collections/165459`, 'album-collection-detail');
+    await snapshotPage(page, `/libraries/${libraryId}/album-collections/165459`, '010-album-collection-detail');
   });
 
   test('artist genres page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/artist-genres`, 'artist-genres');
+    await snapshotPage(page, `/libraries/${libraryId}/artist-genres`, '011-artist-genres');
   });
 
   test('artist genre detail page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/artist-genres/69533`, 'artist-genre-detail');
+    await snapshotPage(page, `/libraries/${libraryId}/artist-genres/69533`, '012-artist-genre-detail');
   });
 
   test('album genres page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/album-genres`, 'album-genres');
+    await snapshotPage(page, `/libraries/${libraryId}/album-genres`, '013-album-genres');
   });
 
   test('album genre detail page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/libraries/${libraryId}/album-genres/69533`, 'album-genre-detail');
+    await snapshotPage(page, `/libraries/${libraryId}/album-genres/69533`, '014-album-genre-detail');
   });
 });
--- tests/pages/4.settings.auth.spec.ts
+++ tests/pages/4.settings.auth.spec.ts
@@ -5,46 +5,46 @@ import { snapshotPage } from '../utils';
 
 test.describe('settings pages match snapshots', () => {
   test('settings page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings`, 'settings');
+    await snapshotPage(page, `/settings`, '001-settings');
   });
 
   test('about page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/about`, 'about');
+    await snapshotPage(page, `/settings/about`, '002-about');
   });
 
   test('appearance page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/appearance`, 'appearance');
+    await snapshotPage(page, `/settings/appearance`, '003-appearance');
   });
 
   test('browse page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/browse`, 'browse');
+    await snapshotPage(page, `/settings/browse`, '004-browse');
   });
 
   test('changelog page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/changelog`, 'changelog');
+    await snapshotPage(page, `/settings/changelog`, '005-changelog');
   });
 
   test('controls page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/controls`, 'controls');
+    await snapshotPage(page, `/settings/controls`, '006-controls');
   });
 
   test('downloads page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/downloads`, 'downloads');
+    await snapshotPage(page, `/settings/downloads`, '007-downloads');
   });
 
   test('general page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/general`, 'general');
+    await snapshotPage(page, `/settings/general`, '008-general');
   });
 
   test('keyboard page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/keyboard`, 'keyboard');
+    await snapshotPage(page, `/settings/keyboard`, '009-keyboard');
   });
 
   test('lastfm page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/lastfm`, 'lastfm');
+    await snapshotPage(page, `/settings/lastfm`, '010-lastfm');
   });
 
   test('sidebar page matches snapshot', async ({ page }) => {
-    await snapshotPage(page, `/settings/sidebar`, 'sidebar');
+    await snapshotPage(page, `/settings/sidebar`, '011-sidebar');
   });
 });
